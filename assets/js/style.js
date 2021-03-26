8dd700278a1bda1b7e101592f324cf9b8dce18f1
anik65 <www.hyper152@gmail.com>
2021-03-26 18:33:02 +0600

    contest
    done

---
+++ assets/js/style.js
@@ -0,0 +1,25 @@
+
+//  sticky nav
+$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 100) {
+        $("nav").addClass("fixed-top");
+    } else {
+        $("nav").removeClass("fixed-top");
+    }
+});
+$('body').scrollspy({ target: ".navbar", offset: 50 });
+$('.carousel').carousel({
+    interval: 3000
+});
+AOS.init();
+//counter
+$('.count').counterUp({
+    delay: 10,
+    time: 1500
+});
+$('.bxslider').bxSlider({
+    controls: false,
+    auto: true,
+
+});
+ 
